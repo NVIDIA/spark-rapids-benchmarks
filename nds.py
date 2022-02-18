269a6f145a8c5bba3518d5ae8113d6e5ad0405f5
Allen Xu <allxu@nvidia.com>
2022-02-18 20:52:08 +0800

    Add necessary patch for data generation
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- nds.py
+++ nds.py
@@ -1,6 +1,21 @@
 import argparse
 import subprocess
 import shutil
+import os
+
+def generate_data(args):
+    # check if hadoop is installed.
+    if (shutil.which('hadoop') is None):
+        raise Exception("No Hadoop binary found in current environment, " +
+                        "please install Hadoop for data generation in cluster.")
+    # submit hadoop MR job to generate data
+    os.chdir('tpcds-gen')
+    subprocess.run(['hadoop', 'jar', 'target/tpcds-gen-1.0-SNAPSHOT.jar',
+        '-d', args.dir, '-p', str(args.parallel), '-s', str(args.scale)])
+
+def generate_query(args):
+    pass
+
 def main():
     parser = argparse.ArgumentParser(description='Argument parser for NDS benchmark options.')
     parser.add_argument('--generate', choices=['data', 'query', 'convert'], required=True,
@@ -12,10 +27,7 @@ def main():
     args = parser.parse_args()
 
     if args.generate == 'data':
-        # check if hadoop is installed.
-        if (shutil.which('hadoop') is None):
-            raise Exception("No Hadoop binary found in current environment, " 
-            +"please install Hadoop for data generation in cluster.")
-        # submit hadoop MR job to generate data
-        subprocess.run(['hadoop', 'jar', 'target/tpcds-gen-1.0-SNAPSHOT.jar',
-                        '-d', args.dir, '-p', args.parallel, '-s', args.scale])
+        generate_data(args)
+
+if __name__ == '__main__':
+    main()
