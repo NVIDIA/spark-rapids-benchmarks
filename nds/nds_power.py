cd9e29fbe87bad6941b88559b266248f5b0b1224
Jihoon Son <ghoonson@gmail.com>
2025-10-27 10:58:00 -0700

    Generalize the query parsing for the queries with multiple sub-queries for NDS (#223)
    * generalize hard-coded query parsing for nds
    
    * skip comments
    
    * cleanup and comment
    
    Signed-off-by: Jihoon Son <ghoonson@gmail.com>
    
    ---------
    
    Signed-off-by: Jihoon Son <ghoonson@gmail.com>

--- nds/nds_power.py
+++ nds/nds_power.py
@@ -43,7 +43,6 @@ from PysparkBenchReport import PysparkBenchReport
 from pyspark.sql import DataFrame
 
 from check import check_json_summary_folder, check_query_subset_exists, check_version
-from nds_gen_query_stream import split_special_query
 from nds_schema import get_schemas
 
 check_version()
@@ -105,12 +104,29 @@ def gen_sql_from_stream(query_stream_file_path):
     for q in all_queries:
         # e.g. "-- start query 32 in stream 0 using template query98.tpl"
         query_name = q[q.find('template')+9: q.find('.tpl')]
-        if 'select' in q.split(';')[1]:
-            part_1, part_2 = split_special_query(q)
-            extended_queries[query_name + '_part1'] = part_1
-            extended_queries[query_name + '_part2'] = part_2
+        queries = q.split(';')
+        non_empty_queries = []
+        for q in queries:
+            q_stripped = q.strip()
+            if q_stripped and not q_stripped.startswith('--'):
+                non_empty_queries.append(q_stripped)
+        if len(non_empty_queries) == 1:
+            # normal query, just one query in the template
+            extended_queries[query_name] = non_empty_queries[0]
         else:
-            extended_queries[query_name] = q
+            # Multiple sub-queries in the query.
+            # We want to update the template name for each part.
+            # See split_special_query function in nds_gen_query_stream.py for more details.
+            head = queries[0].split('\n')[0]
+            query_part = queries[0].replace('.tpl', '_part1.tpl') + ';'
+            extended_queries[f'{query_name}_part1'] = query_part
+            for i in range(len(non_empty_queries) - 1):
+                # We skip the first one since it's already added
+                index = i + 1
+                query_part_index = index + 1
+                query_part = head.replace('.tpl', f'_part{query_part_index}.tpl') + '\n'
+                query_part += non_empty_queries[index] + ';'
+                extended_queries[f'{query_name}_part{query_part_index}'] = query_part
 
     # add "-- start" string back to each query
     for q_name, q_content in extended_queries.items():
