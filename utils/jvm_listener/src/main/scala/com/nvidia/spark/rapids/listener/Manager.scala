903999b10402c2f466ad2a55c09de3cb924b80e1
Chong Gao <res_life@163.com>
2026-02-04 13:46:39 +0800

    Use Java map utils instead of Scala map to avoid compatibility issues
    Signed-off-by: Chong Gao <res_life@163.com>

--- utils/jvm_listener/src/main/scala/com/nvidia/spark/rapids/listener/Manager.scala
+++ utils/jvm_listener/src/main/scala/com/nvidia/spark/rapids/listener/Manager.scala
@@ -18,11 +18,12 @@
 package com.nvidia.spark.rapids.listener
 
 import org.apache.spark.SparkContext
+import java.util.concurrent.ConcurrentHashMap
 
 object Manager {
   /* Manager class to manage all extra customized listeners.
   */
-  private var listeners: Map[String, Listener] = Map.empty[String, Listener]
+  private val listeners = new ConcurrentHashMap[String, Listener]()
   private val spark_listener = new TaskFailureListener()
   private var isRegistered = false
 
@@ -35,19 +36,17 @@ object Manager {
       // We register to the spark listener when the first listener is registered.
       registerSparkListener()
       val uuid = java.util.UUID.randomUUID().toString
-      listeners = listeners.updated(uuid, listener)
+      listeners.put(uuid, listener)
       uuid
     }
   }
 
   def unregister(uuid: String) = {
-    this.synchronized {
-      listeners = listeners.filter { case (k, _) => k != uuid }
-    }
+    listeners.remove(uuid)
   }
 
   def notifyAll(message: String): Unit = {
-    listeners.values.foreach(_.notify(message))
+    listeners.values().forEach(listener => listener.notify(message))
   }
 
   def registerSparkListener() : Unit = {
